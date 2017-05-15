545342c0b41baa4af904bbaf98a5fb0a047c8f96
DatRetr0 <kakapup91@gmail.com>
2017-05-15 18:32:03 +0200

    Add files via upload

---
+++ assets/js/mbt.js
@@ -0,0 +1,9 @@
+/*Animations
+$('.well').addClass('animated zoomIn');
+$('.alert').addClass('animated zoomIn');
+*/
+//window.onload = onLoad();
+function onLoad() {
+  var aud = document.getElementById("backgroundMusic");
+  aud.volume = 0.4;
+}
